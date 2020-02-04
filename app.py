a91d6c88628b93187dbda2879009dbf56ce26f72
theafolayan <theafolayan@gmail.com>
2020-02-04 13:31:47 +0100

    Register form class created

--- app.py
+++ app.py
@@ -17,8 +17,23 @@ def contact():
 def about():
     return render_template('about.html')
 
-@app.route('/articles')
+@app.route('/blog')
 def articles():
-    return render_template('articles.html')
+    return render_template('blog.html')
+
+@app.route('/blog/<string:id/>')
+def blog(id):
+    return render_template('blog-single.html')
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=2, max=35)])
+    email = StringField('Email', [validators.Length(min=6, max=55)])
+    password = PasswordField('Password', [validators.DataRequired(),
+    validators.EqualTo('confirm', message = 'Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm password')
+
 if __name__ == '__main__':
     app.run(debug=True)
+
