1e45ffc9ac356880d2695d091b774eb5fd1565e6
theafolayan <theafolayan@gmail.com>
2020-02-04 13:12:44 +0100

    installed flask-myswl, wtforms, passlib

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect, session, logging
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
 
 app = Flask(__name__)
 @app.route('/')
@@ -13,7 +17,8 @@ def contact():
 def about():
     return render_template('about.html')
 
-
-
+@app.route('/articles')
+def articles():
+    return render_template('articles.html')
 if __name__ == '__main__':
     app.run(debug=True)
